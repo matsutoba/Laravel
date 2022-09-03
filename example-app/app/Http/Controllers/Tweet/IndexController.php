ea111a28e9c3947a02e30a957ccdf622237dafa1
Toshiyuki Matsubara <matsubara_t@outlook.jp>
2022-09-03 11:58:59 +0900

    Chap1

---
+++ example-app/app/Http/Controllers/Tweet/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Tweet;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+
+class IndexController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //return view('tweet.index', ['name' => 'laravel']);
+        return View::make('tweet.index', ['name' => 'larabel']);
+    }
+}
